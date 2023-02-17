039fee3d444e792979e72a2a656687abe7bb226e
cora23tt <mikemcgrat11@gmail.com>
2023-02-17 19:35:49 +0500

    sql file added

---
+++ database/createTables.sql
@@ -0,0 +1,24 @@
+--- psql -U postgres -d bookshelf -a -f createTables.sql
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	key VARCHAR(50) NOT NULL,
+    secret VARCHAR(50) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS books (
+	id SERIAL PRIMARY KEY,
+	isbn VARCHAR(13) NOT NULL,
+	title VARCHAR(150) NOT NULL,
+	cover VARCHAR(250) NOT NULL,
+    author VARCHAR(250) NOT NULL,
+    published INT NOT NULL,
+    pages INT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS user_books (
+	user_id INT NOT NULL,
+	book_id INT NOT NULL,
+	status INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (book_id) REFERENCES books(id));
+
